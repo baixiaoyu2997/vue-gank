406719bc3c8cbe1830f5efe97dcca5aab9001a76
baixiaoyu2997 <bxy2997@sina.com>
2017-06-25 19:36:47 +0800

    更改目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,23 @@ gulp.task('html', function () {
         .pipe(livereload());
 });
 gulp.task('babel', () => {
-    return gulp.src('main.js')
+    return gulp.src('./src/js/main.js')
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('js'));
+        .pipe(gulp.dest('dist/js'));
 });
 //监听
 gulp.task('watch', function () {
     livereload.listen(); //这句不能省，它是用来开启 livereload 服务器
     gulp.watch('index.html', ['html']);
-    gulp.watch("./style/css/*.scss", ["sass"]);
+    gulp.watch("./src/css/*.scss", ["sass"]);
 });
 
 //sass转换成css
 gulp.task("sass", function () {
-    return gulp.src("./style/css/*.scss").pipe(sass({
+    return gulp.src("./src/css/*.scss").pipe(sass({
             outputStyle: "expanded"
         }).on('error', sass.logError))
-        .pipe(gulp.dest("./style/css/"));
+        .pipe(gulp.dest("./dist/css/"));
 });
